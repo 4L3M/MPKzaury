f098235369e6a928ea2673b4358ee6708c2fc133
melamela <272866@student.pwr.edu.pl>
2023-06-01 20:15:21 +0200

    Do poprawy2

---
+++ app/src/main/java/mpkprojekt/CClock.java
@@ -0,0 +1,36 @@
+package mpkprojekt;
+
+public class CClock {
+    int second = 0;
+    int hour = 0;
+    int minute = 0;
+    int currentTime = 0;
+
+    public CClock(int hour, int minute, int second) {
+        this.second = second;
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public String getTime (){
+        return hour + ":" + minute + ":" + second;
+    }
+
+    public void tikTak (){
+        second += 10;
+        if(second >= 60){
+            second %= 60;
+            minute++;
+            if(minute >= 60){
+                minute %= 60;
+                hour++;
+                if(hour > 24){
+                    hour = 0;
+                }
+            }
+        }
+        if(hour < 24){
+        currentTime++;
+        }
+    }
+}
